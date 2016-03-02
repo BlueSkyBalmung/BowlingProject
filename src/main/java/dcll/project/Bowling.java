1110ef57bded5f3a88687886a4065548c4eb0959
BlueSkyBalmung <vincent.heit@gmail.com>
2016-03-02 15:31:48 +0100

    premier Ajout impl Bowling

---
+++ src/main/java/dcll/project/Bowling.java
@@ -0,0 +1,47 @@
+package dcll.project;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bowling
+{
+    ArrayList<Integer> scores;
+   public Bowling(ArrayList<Integer> i){
+        scores=i;
+   }
+
+   public int jouerPartie(){
+        int current=0;
+        int score=0;
+        for(int i=0; i<10 || scores.size()==current;i++){
+            switch(scores.get(current)){
+                case 10:
+                        score+=scores.get(current);
+
+            }
+            current++;
+        }
+       return score;
+   }
+
+   boolean jetsPossible(){
+        for(int i=0; i<scores.size();i++){
+            if(score >10 || score<0){
+                return false;
+            }
+        }
+       return true;
+   }
+
+   int getJets(){
+        return scores;
+   }
+
+   void setJets(ArrayList<Integer> i) {
+       scores = i;
+   }
+}
