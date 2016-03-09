a2dcf86b9d884bde73870bcffe0f161de6834c74
BlueSkyBalmung <vincent.heit@gmail.com>
2016-03-09 14:46:15 +0100

    modification jouerPartie

--- src/main/java/dcll/project/Bowling.java
+++ src/main/java/dcll/project/Bowling.java
@@ -16,17 +16,30 @@ public class Bowling implements IBowling
 
 
     public int jouerPartie(){
-        int current=0;
-        int score=0;
-        for(int i=0; i<10 || scores.size()==current;i++){
-            switch(scores.get(current)){
-                case 10:
-                        score+=scores.get(current);
+        if(jetsPossible()) {
+            int score = 0;
+            int current =0;
+            for (int i = 0; i + 1 < scores.size() || 10 > current; current++) {
+                if (scores.get(i) == 10) {
+                    score += scores.get(i);
+                    score += (scores.size()>(i+1) ? scores.get(i + 1) : 0);
+                    score += (scores.size()>(i+2) ?  scores.get(i + 2) : 0);
+                    i++;
+                } else{
+                     if (scores.size()>(i+1) && (scores.get(i) + scores.get(i + 1)) == 10) {
+                         score += 10;
+                         score += (scores.size()>(i+2) ? scores.get(i + 2) : 0);
 
+                     }else {
+                         score += scores.get(i);
+                         score += (scores.size()>(i+1) ?scores.get(i + 1) :0);
+                     }
+                     i=i+2;
+                }
             }
-            current++;
+            return score;
         }
-       return score;
+        return -1;
    }
 
 
