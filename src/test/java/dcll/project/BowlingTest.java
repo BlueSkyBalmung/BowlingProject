a045da18cdfa86c14c18ea70594d2f855701ab71
TimotheeJammot <tim.jammot@yahoo.fr>
2016-03-09 14:46:35 +0100

    Second commit : tous tests

--- src/test/java/dcll/project/BowlingTest.java
+++ src/test/java/dcll/project/BowlingTest.java
@@ -1,44 +1,101 @@
 package dcll.project;
 
 import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 import java.util.ArrayList;
 
-/**
- * Unit test for simple App.
- */
 public class BowlingTest
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public BowlingTest(String testName)
+    @org.junit.Test
+    public void TestJouerPartie_Strike()
     {
-        super( testName );
+        ArrayList<Integer> jets = new ArrayList<Integer>(12);
+        for (int i = 0; i < 12; i++)
+            jets.add(10);
+        IBowling bowling = new Bowling(jets);
+
+        assert(jets.jouerPartie() == 0);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
+    @org.junit.Test
+    public void TestJouerPartie_NeufEtZero()
     {
-        return new TestSuite( BowlingTest.class );
-    }
-
-    /**
-     *
-     */
-    public void Test()
-    {
-        ArrayList<Integer> jeu = new ArrayList<Integer>(12);
+        ArrayList<Integer> jets = new ArrayList<Integer>(20);
         for (int i = 0; i < 10; i++)
-            jeu.add(0);
-        Bowling bowling = new Bowling(jeu);
-        assertTrue( true );
+        {
+            jets.add(9);
+            jets.add(0);
+        }
+        IBowling bowling = new Bowling(jets);
+
+        assert(jets.jouerPartie() == 90);
+    }
+
+    @org.junit.Test
+    public void TestJouerPartie_CinqEtSpare()
+    {
+        ArrayList<Integer> jets = new ArrayList<Integer>(21);
+        for (int i = 0; i < 10; i++)
+        {
+            jets.add(5);
+            jets.add(10);
+        }
+        jets.add(5);
+        IBowling bowling = new Bowling(jets);
+
+        assert(jets.jouerPartie() == 150);
+    }
+
+    @org.junit.Test
+    public void TestJouerPartieStrikes()
+    {
+        ArrayList<Integer> jets = new ArrayList<Integer>(12);
+        for (int i = 0; i < 12; i++)
+            jets.add(10);
+        IBowling bowling = new Bowling(jets);
+
+        assert(jets.jouerPartie() == 0);
+    }
+
+    @org.junit.Test
+    public void TestSetJets()
+    {
+        ArrayList<Integer> jets = new ArrayList<Integer>(12);
+        for (int i = 0; i < 10; i++)
+            jets.add(0);
+        IBowling bowling = new Bowling(jets);
+
+        jets.set(1,1);
+        bowling.setJeu(jeu);
+
+        assert(bowling.getJets() == jets);
+    }
+
+    @org.junit.Test
+    public void TestSetJets()
+    {
+        ArrayList<Integer> jets = new ArrayList<Integer>(12);
+        for (int i = 0; i < 10; i++)
+            jets.add(0);
+        IBowling bowling = new Bowling(jets);
+
+        assert(bowling.getJets() == jets);
+    }
+
+    @org.junit.Test
+    public void TestVerifierJets()
+    {
+        ArrayList<Integer> jets = new ArrayList<Integer>(12);
+        jets.add(-1);
+        IBowling bowling = new Bowling(jets);
+
+        assertFalse(bowling.verifierJets());
+
+        jets.remove(0);
+        for (int i = 0; i < 10; i++)
+            jets.add(0);
+        bowling.setJets(jets);
+
+        assertTrue(bowling.verifierJets());
     }
 }
